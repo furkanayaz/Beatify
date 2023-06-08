41cad4af12ce3b4be126c1970f923be349206800
= <=>
2023-06-08 14:20:44 +0300

    deleted unnecessary files.

--- app/src/main/java/com/fa/beatify/models/Artist.kt
+++ app/src/main/java/com/fa/beatify/models/Artist.kt
@@ -8,3 +8,36 @@ data class Artist (
     @Expose
     var data: ArrayList<ArtistModel>? = null
 )
+
+data class ArtistModel(
+    @SerializedName("id")
+    @Expose
+    var id: Int? = null,
+    @SerializedName("name")
+    @Expose
+    var name: String? = null,
+    @SerializedName("picture")
+    @Expose
+    var picture: String? = null,
+    @SerializedName("picture_small")
+    @Expose
+    var pictureSmall: String? = null,
+    @SerializedName("picture_medium")
+    @Expose
+    var pictureMedium: String? = null,
+    @SerializedName("picture_big")
+    @Expose
+    var pictureBig: String? = null,
+    @SerializedName("picture_xl")
+    @Expose
+    var pictureXl: String? = null,
+    @SerializedName("radio")
+    @Expose
+    var radio: Boolean? = null,
+    @SerializedName("tracklist")
+    @Expose
+    var tracklist: String? = null,
+    @SerializedName("type")
+    @Expose
+    var type: String? = null
+)
--- app/src/main/java/com/fa/beatify/models/Genre.kt
+++ app/src/main/java/com/fa/beatify/models/Genre.kt
@@ -8,3 +8,30 @@ data class Genre(
     @Expose
     var data : ArrayList<GenreModel>? = null
 )
+
+data class GenreModel(
+    @SerializedName("id")
+    @Expose
+    var id: Int? = null,
+    @SerializedName("name")
+    @Expose
+    var name: String? = null,
+    @SerializedName("picture")
+    @Expose
+    var picture: String? = null,
+    @SerializedName("picture_small")
+    @Expose
+    var pictureSmall: String? = null,
+    @SerializedName("picture_medium")
+    @Expose
+    var pictureMedium: String? = null,
+    @SerializedName("picture_big")
+    @Expose
+    var pictureBig: String? = null,
+    @SerializedName("picture_xl")
+    @Expose
+    var pictureXl: String? = null,
+    @SerializedName("type")
+    @Expose
+    var type: String? = null
+)
