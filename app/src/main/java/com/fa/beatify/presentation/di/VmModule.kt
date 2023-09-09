434b10c50780faf0d97f1a356ebcac8d2eb286da
Furkan Ayaz <furkanayaz.dev@gmail.com>
2023-09-09 16:31:18 +0300

    Updated dependencies of some libraries

--- app/src/main/java/com/fa/beatify/presentation/di/VmModule.kt
+++ app/src/main/java/com/fa/beatify/presentation/di/VmModule.kt
@@ -6,54 +6,17 @@ import com.fa.beatify.presentation.artist_detail.ArtistDetailVM
 import com.fa.beatify.presentation.artists.ArtistsVM
 import com.fa.beatify.presentation.music_categories.MusicCategoriesVM
 import com.fa.beatify.presentation.music_likes.LikesVM
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val vmModule: Module = module {
 
-    viewModel {
-        MainActivityVM()
-    }
-
-    viewModel {
-        LikesVM(
-            allLikesUseCase = get(),
-            deleteLikeUseCase = get()
-        )
-    }
-
-    viewModel {
-        MusicCategoriesVM(
-            networkConnection = get(),
-            allGenresUseCase = get()
-        )
-    }
-
-    viewModel {
-        ArtistsVM(
-            networkConnection = get(),
-            allArtistsUseCase = get()
-        )
-    }
-
-    viewModel {
-        ArtistDetailVM(
-            networkConnection = get(),
-            allAlbumsUseCase = get(),
-            dateRepo = get()
-        )
-    }
-
-    viewModel {
-        AlbumDetailVM(
-            networkConnection = get(),
-            allTracksUseCase = get(),
-            insertLikeUseCase = get(),
-            deleteLikeUseCase = get(),
-            imageRepo = get(),
-            durationRepo = get()
-        )
-    }
+    viewModelOf(::MainActivityVM)
+    viewModelOf(::LikesVM)
+    viewModelOf(::MusicCategoriesVM)
+    viewModelOf(::ArtistsVM)
+    viewModelOf(::ArtistDetailVM)
+    viewModelOf(::AlbumDetailVM)
 
 }
